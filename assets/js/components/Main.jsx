2b5942280424a02b337d5f46f0060de6fbfe135a
Josh Rowley <joshua.f.rowley@gmail.com>
2016-05-07 04:51:56 -0400

    iterate through logins from state

--- assets/js/components/Main.jsx
+++ assets/js/components/Main.jsx
@@ -2,12 +2,20 @@ var React = require('react');
 var Card = require('./Card.jsx');
 
 var Main = React.createClass({
+  getInitialState: function () {
+    return {
+      logins: ['joshrowley', 'snags88', 'adamjonas']
+    };
+  },
+
   render: function () {
+    var cards = this.state.logins.map(function (login) {
+      return (<Card login={login} />);
+    });
+
     return (
       <div>
-        <Card login='joshrowley'/>
-        <Card login='snags88'/>
-        <Card login='adamjonas'/>
+        { cards }
       </div>
     );
   }
