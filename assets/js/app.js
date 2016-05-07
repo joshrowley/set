db6f712bb6dbc28ea49ca03913b82f9f95a23d1c
Josh Rowley <joshua.f.rowley@gmail.com>
2016-05-07 04:01:04 -0400

    incrementing button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var HelloWorld = require('./components/HelloWorld.jsx');
+var Button = require('./components/Button.jsx');
 
-ReactDOM.render(<HelloWorld />, document.getElementById('root'));
+ReactDOM.render(<Button />, document.getElementById('root'));
---
+++ assets/js/components/Button.jsx
@@ -0,0 +1,23 @@
+var React = require('react');
+
+var Button = React.createClass({
+  getInitialState: function () {
+    return {
+      counter: 0
+    };
+  },
+
+  handleClick: function () {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  },
+
+  render: function () {
+    return (
+      <button onClick={this.handleClick}>{ this.state.counter }</button>
+    );
+  }
+});
+
+module.exports = Button;
